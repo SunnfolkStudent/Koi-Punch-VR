8a84c479400fdd3a4dd1b52ad4541aa27af40b39
EOppedal <erlend.oppedal@gmail.com>
2024-01-05 18:43:21 +0100

    Fish trajectory improved

--- Assets/Scripts/InDevelopment/Trajectory/FishTrajectory.cs
+++ Assets/Scripts/InDevelopment/Trajectory/FishTrajectory.cs
@@ -1,5 +1,6 @@
 using System;
 using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 
 namespace InDevelopment.Trajectory
@@ -10,7 +11,7 @@ namespace InDevelopment.Trajectory
         [SerializeField] private float fishSpeed;
 
         private Rigidbody _rigidbody;
-        private const float Gravity = 9.8f;
+        private const float Gravity = 9.81f;
         
         private void Start()
         {
@@ -19,11 +20,13 @@ namespace InDevelopment.Trajectory
             var fishPosition = transform.position;
             var targetPosition = playerTransform.position;
             
-            var distance = targetPosition.x - fishPosition.x;
+            var i = targetPosition- fishPosition;
+            var distance = math.sqrt(i.x * i.x + i.z * i.z);
+
             var altitudeDifference = fishPosition.y - targetPosition.y;
 
-            RotateTowardsTarget(targetPosition);
-            _rigidbody.velocity = TrajectoryAngleFromSpeedDistanceAltitude(fishSpeed, distance, altitudeDifference);
+            // RotateTowardsTarget(targetPosition);
+            TrajectoryAngleFromSpeedDistanceAltitude(fishSpeed, distance, altitudeDifference);
         }
 
         private void RotateTowardsTarget(Vector3 targetPosition)
@@ -33,13 +36,15 @@ namespace InDevelopment.Trajectory
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
         
-        private Vector3 TrajectoryAngleFromSpeedDistanceAltitude(float speed, float dist, float alt)
+        private void TrajectoryAngleFromSpeedDistanceAltitude(float speed, float dist, float alt)
         {
             var angleInRadians = (float)Math.Atan((NumberExponent(speed, 2) + Math.Sqrt(NumberExponent(speed, 4) - Gravity * 
                                 ((Gravity * NumberExponent(dist, 2)) + (2 * alt * NumberExponent(speed, 2))))) / (Gravity * dist));
             
-            return (new Vector3(0, Mathf.Sin(angleInRadians), 0)
-                 + transform.forward * Mathf.Cos(angleInRadians)) * speed;
+            var vx = Mathf.Cos(angleInRadians) * speed;
+            var vy = Mathf.Sin(angleInRadians) * speed;
+            
+            example(vx, vy);
         }
 
         private static float NumberExponent(float number, int exponent)
@@ -47,6 +52,16 @@ namespace InDevelopment.Trajectory
             var num = number;
             for (var i = 1; i < exponent; i++) number *= num;
             return number;
+        }
+
+
+        private void example(float sideSpeed, float upSpeed)
+        {
+            Vector3 playerDirection = (playerTransform.position - transform.position).normalized;
+
+            playerDirection *= sideSpeed;
+
+            _rigidbody.velocity = new Vector3(playerDirection.x, upSpeed, playerDirection.z);
         }
     }
 }
