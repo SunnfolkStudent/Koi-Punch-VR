6c3c964a1a164f4223d4b49fe5a3173e2bb8dcf2
Tusda <marcus.wiborg@outlook.com>
2024-01-05 12:16:30 +0100

    Added ZenMetre and attack field spawning

---
+++ Assets/Scripts/InDevelopment/Zen/AttackFieldScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackFieldScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathTimer());
+    }
+    
+    private IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
