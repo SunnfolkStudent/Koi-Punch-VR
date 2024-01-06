8c676288f97f42a798ab3e3e226474ca838a06dd
Benjameepmeep <benjamin.arctander@gmail.com>
2024-01-06 16:18:35 +0100

    SpawnAreaCircle visual improvement, kinda

--- Assets/Scripts/InDevelopment/Fish/SpawnAreaCircle.cs
+++ Assets/Scripts/InDevelopment/Fish/SpawnAreaCircle.cs
@@ -6,19 +6,19 @@ namespace InDevelopment.Fish
     [RequireComponent(typeof(LineRenderer))]
     public class SpawnAreaCircle : MonoBehaviour 
     {
-        [Range(0,50)]
-        public int segments = 50;
-        [Range(0,5)]
-        public float xradius = 5;
-        [Range(0,5)]
-        public float yradius = 5;
+        [Range(0,180)]
+        private int _segments = 180;
+        [Range(0,2)]
+        public float xradius = 1;
+        [Range(0,2)]
+        public float yradius = 1
         LineRenderer _line;
 
         void Start ()
         {
             _line = gameObject.GetComponent<LineRenderer>();
 
-            _line.positionCount = (segments + 1);
+            _line.positionCount = (_segments + 1);
             _line.useWorldSpace = false;
             CreatePoints ();
         }
@@ -36,14 +36,14 @@ namespace InDevelopment.Fish
 
             float angle = 20f;
 
-            for (int i = 0; i < (segments + 1); i++)
+            for (int i = 0; i < (_segments + 1); i++)
             {
                 x = Mathf.Sin (Mathf.Deg2Rad * angle) * xradius;
                 y = Mathf.Cos (Mathf.Deg2Rad * angle) * yradius;
 
                 _line.SetPosition (i,new Vector3(x,y,0) );
 
-                angle += (360f / segments);
+                angle += (360f / _segments);
             }
         }
     }
