72391c97f3a77fe9eb9a9ca628fd20594438b0e8
Benjameepmeep <benjamin.arctander@gmail.com>
2024-01-06 11:40:24 +0100

    FishSpawnManager Script with To-Do List

---
+++ Assets/Scripts/InDevelopment/Fish/FishSpawnManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InDevelopment.Fish
+{
+    // This script will be set on an outside gameObject, and aids in spawning the Fish.
+    public class FishSpawnManager : MonoBehaviour
+    {
+        // TODO: Object Pooling - Enabling and Disabling gameObjects, and duplicating if there's a need for more.
+            // TODO: When fish are set to Inactive, fish variables will be reset.
+
+            // Variables the FishSpawnManager handles:
+
+            // TODO: Spawn Frequency of fish, increased over time === Gradually increasing.
+                // Use Animation Curves inside Unity in Inspector, and make the code read off of the graph there
+                // to gradually increase fish spawn speed.
+
+            // TODO: Spawn Area (with offset) a.k.a. Spawn Position & Fish Rotation === Random Weighted.
+                // Use a "weightTable" to help track where former/recent fish have spawned, and avoid the most recently used spawns.
+                // Based on input from weightTable, choose spawn area, and then offset position, and randomly weighted rotation on fish being sent.
+            
+            // TODO: Spawned Fish Properties; Size&Mass + Colour/Skin. === Random Weighted.
+                // Use a "weightTable" to help track what former/recent fish properties have been, and avoid the most recently used properties.
+                // Based on input from weightTable, choose size&mass, and colour/skin. 
+            
+        // TODO: FishSpawnManager will use the static functions created by FishTrajectory, to help launch the fish initially.
+        
+        // TODO: Object Pooling - When receiving input from a fish object about colliding,
+        // TODO: Turn same fish Inactive/Despawn it with a function from this FishSpawnManager.
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
