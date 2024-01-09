075eafbd0d88c012a33ee49de053aded8dfa4852
Tusda <marcus.wiborg@outlook.com>
2024-01-09 10:09:55 +0100

    Special attack script

--- Assets/Scripts/InDevelopment/Zen/SpecialAttackScript.cs
+++ Assets/Scripts/InDevelopment/Zen/SpecialAttackScript.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.OpenVR;
 using UnityEngine;
 
 public class SpecialAttackScript : MonoBehaviour
 {
+    public bool chargingPunch;
+    public bool punchCharged;
+    private float _timeToCharge = 5;
+    private float _chargeTimer;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,18 @@ public class SpecialAttackScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (chargingPunch)
+        {
+            _chargeTimer -= Time.deltaTime;
+            if (_chargeTimer <= 0)
+            {
+                chargingPunch = false;
+                punchCharged = true;
+            }
+        }
+        else if (_chargeTimer < _timeToCharge)
+        {
+            _chargeTimer = _timeToCharge;
+        }
     }
 }
