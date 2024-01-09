45f5ba535f1a0e1f6a83a0e43b648809fa566056
Tusda <marcus.wiborg@outlook.com>
2024-01-09 14:30:15 +0100

    Zen visually goes down when charging special attack and zen is removed if charging is cut off early

--- Assets/Scripts/InDevelopment/Zen/SpecialAttackScript.cs
+++ Assets/Scripts/InDevelopment/Zen/SpecialAttackScript.cs
@@ -1,36 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.XR.OpenVR;
 using UnityEngine;
+
 
 public class SpecialAttackScript : MonoBehaviour
 {
-    public bool chargingPunch;
-    public bool punchCharged;
-    private float _timeToCharge = 5;
     private float _chargeTimer;
-    
-    // Start is called before the first frame update
+    private float _timeToCharge = 10f;
+    private float _zenLostPerSecond;
+    public bool chargingPunch;
+    public static bool punchCharged;
+    public static float punchForce;
+
+
     void Start()
     {
+        _chargeTimer = _timeToCharge;
         
+        _zenLostPerSecond = 300f / _timeToCharge;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (chargingPunch)
         {
-            _chargeTimer -= Time.deltaTime;
-            if (_chargeTimer <= 0)
+            ZenMetreManager.Instance.zenMetreValue -= _zenLostPerSecond * Time.unscaledDeltaTime;
+            
+            if (ZenMetreManager.Instance.zenMetreValue <= 0 && ZenMetreManager.Instance.zenLevel != 1)
+            {
+                ZenMetreManager.Instance.zenLevel--;
+                ZenMetreManager.Instance.zenMetreValue = 100f;
+            }
+            
+            else if (ZenMetreManager.Instance.zenMetreValue <= 0 && ZenMetreManager.Instance.zenLevel == 1)
             {
                 chargingPunch = false;
                 punchCharged = true;
+                ZenMetreManager.Instance.zenAttackActive = false;
+                ZenMetreManager.Instance.zenMetreValue = 0f;
+                CalculatePunchForce();
             }
+            
+            ZenMetreVisualManager.Instance.UpdateZenBar(ZenMetreManager.Instance.zenLevel, ZenMetreManager.Instance.zenMetreValue);
         }
-        else if (_chargeTimer < _timeToCharge)
-        {
-            _chargeTimer = _timeToCharge;
-        }
+    }
+    
+    private void CalculatePunchForce()
+    {
+        punchForce = (((4 - ZenMetreManager.Instance.zenLevel) * 100) - ZenMetreManager.Instance.zenMetreValue);
+        ZenMetreVisualManager.Instance.UpdateZenBar(1, 0f);
+        ZenMetreVisualManager.Instance.UpdateZenBar(2, 0f);
+        ZenMetreVisualManager.Instance.UpdateZenBar(3, 0f);
     }
 }
