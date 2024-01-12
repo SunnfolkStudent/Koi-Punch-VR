72b856fd68172a7fe88180412df3a290c8dfafce
a penguin <122266606+IsItPenguins@users.noreply.github.com>
2024-01-12 14:01:40 +0100

    Fmod Update

---
+++ Assets/Scripts/InDevelopment/Examplesoundscript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+public class Examplesoundscript : MonoBehaviour
+{
+    [SerializeField] private EventReference() exampleVariable;
+    FMODManager.instance.PlayOneShot(exampleVariable, this.transform.position);
+}
