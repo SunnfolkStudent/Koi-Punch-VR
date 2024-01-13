226a9109e9a4e82ad8e9c42013b192655ba72c5a
Tusda <marcus.wiborg@outlook.com>
2024-01-13 13:03:26 +0100

    Comments in events

--- Assets/Scripts/InDevelopment/Fish/EventManager.cs
+++ Assets/Scripts/InDevelopment/Fish/EventManager.cs
@@ -10,12 +10,29 @@ namespace InDevelopment.Fish
         public static Event FishSpawning = SpawnFishDebug;
         public static Event FishSpawningAtMaxRate = SpawnFishDebug;
         public static Event StopFishSpawning = LevelOverDebug;
-        public static Event ZenBar1Full;
-        public static Event BossPhase0;
+        
+        //This event is invoked when the first zen bar is full and the boss should spawn when hit time will stop
+        public static Event ZenBar1Full; 
+        
+        //This event is invoked only if you fail one of the boss phases and go back to phase zero or when you hit the boss with your final move
+        public static Event BossPhase0; 
+        
+        //This event is invoked when the boss is hit with the first move and time is stopped.
+        //This is the phase where weak points will spawn
         public static Event BossPhase1;
+        
+        //This event is invoked when you clear phase one.
+        //Weak points will stop spawning and it is time for the triple score punch out where you punch the boss as much as possible
         public static Event BossPhase2;
+        
+        //This event is invoked when you clear all both stages and the special attack is ready.
         public static Event BossPhase3;
+        
+        //This event is invoked after you charge your punch.
+        //There will be a variable in SpecialAttackScript called punchForce that is a float between 0-300 and where 300 is the max.
+        //This variable tells you how much the special attack was charged before the buttons were let go
         public static Event ZenPunchReady;
+        
         public static Event BossDefeated = BossDefeatedDebug;
         #endregion
         
