d7dda1ade8f8b7977a8ee15d5c02a408f76d743f
PremiumKatt <amundtomrenslemmen@gmail.com>
2024-01-13 12:26:21 +0100

    Introtrigger

--- Assets/Scripts/InDevelopment/Amund_Scripts/Scenes/SceneController.cs
+++ Assets/Scripts/InDevelopment/Amund_Scripts/Scenes/SceneController.cs
@@ -1,28 +1,30 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
-public class TitleCard : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
-    private Keyboard _keyboard;
+    [SerializeField] private string IntroScene;
     [SerializeField] private Transform Goal;
+    [SerializeField] private GameObject Title;
+    [SerializeField] private GameObject IntroTrigger;
     private float speed;
     private float time;
     private bool startMoving;
     [SerializeField] private AudioClip musicLength;
-
     [SerializeField] private AnimationCurve titleSpeed;
-    
+
     private void Start()
     {
-        _keyboard = new Keyboard();
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(IntroScene, LoadSceneMode.Additive);
     }
-    
+
     private void Update()
     {
-        _keyboard = Keyboard.current;
-        //Change to something automatic
-        if (_keyboard.enterKey.wasPressedThisFrame)
+        
+        if (IntroTrigger == null)
         {
             print("Moving");
             startMoving = true;
@@ -31,18 +33,17 @@ public class TitleCard : MonoBehaviour
         if (!startMoving) return;
         speed = titleSpeed.Evaluate(time);
         time += Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, Goal.transform.position, speed);
-        if (transform.position == Goal.transform.position)
+        Title.transform.position = Vector3.MoveTowards(Title.transform.position, Goal.transform.position, speed);
+        if (Title.transform.position == Goal.transform.position)
         {
             StartCoroutine(PlayIntro());
         }
     }
     
-
     private IEnumerator PlayIntro()
     {
         print("Play Music"); //Play music here
         yield return new WaitForSeconds(musicLength.length);
-        //Send to main menu
+        SceneManager.UnloadSceneAsync(IntroScene);
     }
 }
