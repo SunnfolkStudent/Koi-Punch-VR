f286ae09bfbed00dd18d4dbc72fa7b4d275ad8cf
EOppedal <erlend.oppedal@gmail.com>
2024-01-15 16:53:29 +0100

    Boss launch

--- Assets/Scripts/InDevelopment/Fish/Boss.cs
+++ Assets/Scripts/InDevelopment/Fish/Boss.cs
@@ -9,17 +9,22 @@ namespace InDevelopment.Fish
     public class Boss : MonoBehaviour, IPunchable
     {
         [SerializeField] private Transform player;
-        [SerializeField] private BossPhase bossState;
+        [SerializeField] private float zenPerHitPhase2 = 5f;
+        private static BossPhase _currentBossState;
+        private float _score;
 
+        #region ---Initialization---
         private void Start()
         {
             EventManager.StartBossPhase0 += Phase0;
             EventManager.BossPhase0Completed += Phase0Completed;
+            EventManager.BossPhaseSuccessful += PhaseSuccessful;
             EventManager.StartBossPhase1 += Phase1;
             EventManager.StartBossPhase2 += Phase2;
             EventManager.StartBossPhase3 += Phase3;
         }
-
+        #endregion
+        
         #region ---PhaseProperties---
         private enum BossPhase
         {
@@ -48,69 +53,89 @@ namespace InDevelopment.Fish
             }
         }
         #endregion
-
-        #region ---BossPhases---
+        
+        #region ---BossPhasesStart---
         private void Phase0()
         {
+            _currentBossState = BossPhase.Phase0;
+            
             var spawnPos = FishSpawnAreas.GetNextFishSpawnPosition();
             var playerPosition = player.position;
             var rigidities = GetComponentsInChildren<Rigidbody>();
             var velocity2D = FishTrajectory.TrajectoryVelocity2DFromInitialSpeed(spawnPos, playerPosition, 35f);
             FishSpawnManager.LaunchRigiditiesDirectionWithVelocity(rigidities, playerPosition, velocity2D);
         }
-
+        
         private static void Phase1()
         {
-            // TODO: spawn weak-points
+            _currentBossState = BossPhase.Phase1;
         }
-
+        
         private static void Phase2()
         {
-            // TODO: count hits
+            _currentBossState = BossPhase.Phase2;
         }
-
-        private static void Phase2Hit()
-        {
-            // var scorePerHit = 5;
-            // _score += scorePerHit;
-        }
-
+        
         private static void Phase3()
         {
-            throw new System.NotImplementedException();
+            _currentBossState = BossPhase.Phase3;
         }
         #endregion
         
         #region ---PhaseCompletion---
         private static void Phase0Completed()
         {
-            ZenMetreManager.Instance.TimeStop();
             Phase.FirstOrDefault(keyValuePair => keyValuePair.Value.score == 0).Value.Event.Invoke();
         }
 
-        private static void BossPhaseCompleted(BossPhase bossPhase, float score)
+        private static void PhaseSuccessful()
         {
-            Phase[bossPhase - 1].score = score;
-            Phase[bossPhase].Event.Invoke();
+            Phase[_currentBossState].score = ZenMetreManager.Instance.zenMetreValue;
+            // _currentBossState++;
+            Phase[_currentBossState++].Event.Invoke();
         }
         #endregion
 
+        #region ---PunchBoss--
         public void PunchObject(ControllerManager controllerManager, string fistUsed)
         {
-            switch(bossState){
+            switch(_currentBossState){
                 case BossPhase.Phase0:
                     EventManager.BossPhase0Completed.Invoke();
+                    break;
+                case BossPhase.Phase1:
+                    Debug.Log("Can't hit the boss directly in Phase1");
                     break;
                 case BossPhase.Phase2:
                     Phase2Hit();
                     break;
                 case BossPhase.Phase3:
-                    // TODO: make boss fly away when hit by KOI-PUNCH
+                    Phase3Hit(controllerManager, fistUsed);
                     break;
                 default:
-                    Debug.Log("Can't hit the boss in this Phase");
+                    Debug.Log("Boss not in valid Phase");
                     break;
             }
         }
+        
+        private void Phase2Hit()
+        {
+            ZenMetreManager.Instance.AddHitZen(zenPerHitPhase2);
+        }
+        
+        private void Phase3Hit(ControllerManager controllerManager, string fistUsed)
+        {
+            if (!SpecialAttackScript.punchCharged) return;
+            
+            var controllerVelocity = fistUsed == "LeftFist" ? controllerManager.leftControllerVelocity : controllerManager.rightControllerVelocity;
+            var zenPunchForce = SpecialAttackScript.punchForce * controllerVelocity.normalized;
+            var force = controllerVelocity + zenPunchForce;
+            
+            GetComponent<Rigidbody>().AddForce(force);
+            
+            var totalScore = Phase.Sum(pair => pair.Value.score);
+            // TODO: ScoreManager.score += totalScore
+        }
+        #endregion
     }
 }
