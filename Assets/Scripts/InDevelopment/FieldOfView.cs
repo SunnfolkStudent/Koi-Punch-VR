5186aa67e6e6abd8147b9bc466c1aca44b600b30
Benjameepmeep <benjamin.arctander@gmail.com>
2024-01-16 21:51:04 +0100

    Added FOV Script - needs proper implementation

---
+++ Assets/Scripts/InDevelopment/FieldOfView.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InDevelopment
+{
+    public class FieldOfView : MonoBehaviour
+    {
+        public float viewRadius;
+        [Range(0, 360)] public float viewAngle;
+
+        public LayerMask spawnAreaMask;
+        public LayerMask obstacleMask;
+
+        [HideInInspector] public List<Transform> visibleSpawnAreas = new List<Transform>();
+
+        private void Start()
+        {
+            StartCoroutine(FindSpawnAreaWithDelay(0.1f));
+        }
+        
+        IEnumerator FindSpawnAreaWithDelay(float delay)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(delay);
+                FindVisibleSpawnAreas();
+            }
+        }
+        void FindVisibleSpawnAreas()
+        {
+            visibleSpawnAreas.Clear();
+            int maxSpawnAreasInView = 10;
+            Collider[] spawnAreasInViewRadius = new Collider[maxSpawnAreasInView];
+            var spawnAreasInFOV = Physics.OverlapSphereNonAlloc(transform.position, viewRadius, spawnAreasInViewRadius,
+                spawnAreaMask);
+
+            for (int i = 0; i < spawnAreasInFOV; i++)
+            {
+                Transform spawnArea = spawnAreasInViewRadius[i].transform;
+                Vector3 dirToSpawnArea = (spawnArea.position - transform.position).normalized;
+                if (Vector3.Angle(transform.forward, dirToSpawnArea) < viewAngle / 2)
+                {
+                    float distanceToSpawnArea = Vector3.Distance(transform.position, spawnArea.position);
+
+                    if (!Physics.Raycast(transform.position, dirToSpawnArea, distanceToSpawnArea, obstacleMask))
+                    {
+                        visibleSpawnAreas.Add(spawnArea);
+                    }
+                }
+            }
+        }
+        public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+        {
+            if (!angleIsGlobal)
+            {
+                angleInDegrees += transform.eulerAngles.y;
+            }
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
+    }
+}
--- Assets/Scripts/InDevelopment/Fish/Trajectory/MovePlayer.cs
+++ Assets/Scripts/InDevelopment/Fish/Trajectory/MovePlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using FinalScripts.Fish;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,9 +7,18 @@ namespace InDevelopment.Fish.Trajectory
 {
     public class MovePlayer : MonoBehaviour
     {
-        // TODO: Remove this script, it is only for testing
+        // TODO: Remove this script, it is only for testing, but wait until boss spawning from spawn area in FOV is done!
         [SerializeField] private float speed = 0.5f;
-        
+
+        private Rigidbody _rigidbody;
+        private Camera _viewCamera;
+        private Vector3 _velocity;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _viewCamera = Camera.main;
+        }
         private void FixedUpdate()
         {
             transform.position += new Vector3(-Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal")) * speed;
@@ -31,6 +41,11 @@ namespace InDevelopment.Fish.Trajectory
 
         private void Update()
         {
+            Vector3 mousePos = _viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+                _viewCamera.transform.position.y));
+            transform.LookAt(mousePos + Vector3.up * transform.position.y);
+            _velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed;
+            
             if (Keyboard.current.lKey.wasPressedThisFrame)
             {
                 EventManager.SpawnFish.Invoke();
