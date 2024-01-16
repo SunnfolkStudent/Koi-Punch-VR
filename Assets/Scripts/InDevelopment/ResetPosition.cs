a7fa35c0e551fdbb656bc244b69a52ab4e792fa5
UncoloredPlane3 <fredrik.haaven@gmail.com>
2024-01-16 10:24:18 +0100

    made a reset position button

---
+++ Assets/Scripts/InDevelopment/ResetPosition.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using Unity.XR.CoreUtils;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class ResetPosition : MonoBehaviour
+{
+    [SerializeField] private InputActionReference leftResetButton;
+    [SerializeField] private InputActionReference rightResetButton;
+
+    private bool _resetPosLeft;
+    private bool _resetPosRight;
+    
+    public Transform head; 
+    public Transform origin;
+    public Transform target;
+    
+    [ContextMenu("Test Starting Pos")]
+    public void ResetPos()
+    {
+        Vector3 offset = head.position - origin.position;
+        offset.y = 0;
+        origin.position = target.position - offset;
+
+        Vector3 targetForward = target.forward;
+        targetForward.y = 0;
+        Vector3 cameraForward = head.forward;
+        cameraForward.y = 0;
+
+        float angle = Vector3.SignedAngle(cameraForward, targetForward, Vector3.up);
+        
+        origin.RotateAround(head.position, Vector3.up, angle);
+    }
+    void Start()
+    {
+       
+    }
+
+    private void Update()
+    {
+        _resetPosLeft = leftResetButton.action.IsPressed();
+        _resetPosRight = rightResetButton.action.IsPressed();
+
+        if (_resetPosLeft || _resetPosRight)
+        {
+            ResetPos();
+        }
+    }
+}
