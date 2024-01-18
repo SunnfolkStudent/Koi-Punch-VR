9b34379d651eaad144af91eed3cd533f1075548a
a penguin <122266606+IsItPenguins@users.noreply.github.com>
2024-01-18 14:53:54 +0100

    Changes

--- Assets/Scripts/InDevelopment/Subtitles/SubtitleEventManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class SubtitleEventManager
-{
-    public delegate void SubtitleEvents(string subtitle);
-    public static SubtitleEvents PlaySubtitle;
-}
