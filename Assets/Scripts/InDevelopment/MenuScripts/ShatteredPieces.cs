4a56cf42e711d6b3814b96866438ff80f6465f8b
Kris-Chil <Kristinemchilson@gmail.com>
2024-01-19 11:40:29 +0100

    menu layout

--- Assets/Scripts/InDevelopment/MenuScripts/ShatteredPieces.cs
+++ Assets/Scripts/InDevelopment/MenuScripts/ShatteredPieces.cs
@@ -17,7 +17,17 @@ public class ShatteredPieces : MonoBehaviour
     {
         if (_rb == null) return;
         if(other.gameObject.CompareTag("Ground")) return;
-        _rb.AddForce(other.rigidbody.velocity*500);
+        
+        _rb.AddForce(other.rigidbody.velocity * 250);
+        
+        /*if(other.gameObject.CompareTag("RightFist") || other.gameObject.CompareTag("LeftFist"))
+        {
+            _rb.AddForce(other.rigidbody.velocity * 250);
+        }
+        else
+        {
+            _rb.AddForce(other.rigidbody.velocity * 200);
+        }*/
     }
     
     
